dee1977462dfcec5080e1453205b64e7428049e3
uiu cavalheiro <wil.cavalheiro@gmail.com>
2013-09-17 15:42:02 -0300

    footer-social-rearrangement

--- footer.php
+++ footer.php
@@ -27,21 +27,16 @@
 		</div>
 		<div class="three columns">
 			<div class="social">
-				<div class="social-links">
-					<div class="social-container">
-						<div class="social-content">
-							<a href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751?fref=ts" class="facebook lsf" rel="external" target="_blank">&#xE047;</a>
-							<a href="http://instagram.com/arteforadomuseu" class="instagram lsf" rel="external" target="_blank">&#xE155;</a>
-							<a href="https://twitter.com/arteforadomuseu" class="twitter lsf" rel="external" target="_blank">&#xE12f;</a>
-							<a href="http://www.youtube.com/arteforadomuseu" class="youtube lsf" rel="external" target="_blank">&#xE141;</a>
-						</div>
-					</div>
-					<span class="social-toggler lsf">&#xE118;</span>
-				</div>
 				<div class="social-interaction">
 					<a href="https://twitter.com/arteforadomuseu" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-show-screen-name="false">Seguir @arteforadomuseu</a>
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 					<div class="fb-like" data-href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true" data-font="verdana" data-colorscheme="dark"></div>
+				</div>
+				<div class="social-links">
+					<a href="https://www.facebook.com/bhasia2013" class="facebook lsf" rel="external" target="_blank">&#xE047;</a>
+					<a href="http://instagram.com/arteforadomuseu" class="instagram lsf" rel="external" target="_blank">&#xE155;</a>
+					<a href="https://twitter.com/arteforadomuseu" class="twitter lsf" rel="external" target="_blank">&#xE12f;</a>
+					<a href="http://www.youtube.com/arteforadomuseu" class="youtube lsf" rel="external" target="_blank">&#xE141;</a>
 				</div>
 			</div>
 		</div>
