a961390ba28be3ff45829b6196894f14d501057d
uiu cavalheiro <wil.cavalheiro@gmail.com>
2013-10-01 18:11:46 -0300

    top menu placement

--- header.php
+++ header.php
@@ -69,11 +69,7 @@
 				<div id="masthead-nav">
 					<div class="clearfix">
 						<nav id="main-nav">
-
-							<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>
-
 							<ul>
-								
 								<li><a href="<?php echo afdm_artists_get_archive_link(); ?>"><?php _e('Artists', 'arteforadomuseu'); ?></a></li>
 								<!--<li><a href="<?php echo afdm_artguides_get_archive_link(); ?>"><?php _e('Art guides', 'arteforadomuseu'); ?></a></li>
 								<?php
@@ -104,6 +100,7 @@
 									</li>
 								<?php endif; ?>-->
 							</ul>
+							<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>
 						</nav>
 						<?php get_search_form(); ?>
 					</div>
