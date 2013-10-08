c5234edde342cd82899db4410295736a48512d38
uiu cavalheiro <wil.cavalheiro@gmail.com>
2013-10-08 02:37:14 -0300

    bubble artists remove

--- content-marker-bubble.php
+++ content-marker-bubble.php
@@ -7,6 +7,6 @@
 <h4><?php the_title(); ?></h4>
 <?php if(afdm_has_artist()) : ?>
 	<div class="meta">
-		<p class="artists"><span class="lsf">user</span> <?php _e('Artists', 'arteforadomuseu'); ?>: <?php afdm_the_artist(); ?></p>
+		<p class="artists"> <?php afdm_the_artist(); ?></p>
 	</div>
 <?php endif; ?>
