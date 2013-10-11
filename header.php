696cbde217f15d4fb4569eeea40d471254f7e9c4
uiu cavalheiro <wil.cavalheiro@gmail.com>
2013-10-11 04:21:35 -0300

    header artists remove

--- header.php
+++ header.php
@@ -71,8 +71,8 @@
 						<nav id="main-nav">
 							<?php wp_nav_menu(array('theme_location' => 'header_menu')); ?>
 							<ul>
-								<li><a href="<?php echo afdm_artists_get_archive_link(); ?>"><?php _e('Artists', 'arteforadomuseu'); ?></a></li>
-								<!--<li><a href="<?php echo afdm_artguides_get_archive_link(); ?>"><?php _e('Art guides', 'arteforadomuseu'); ?></a></li>
+								<!--<li><a href="<?php echo afdm_artists_get_archive_link(); ?>"><?php _e('Artists', 'arteforadomuseu'); ?></a></li>
+								<li><a href="<?php echo afdm_artguides_get_archive_link(); ?>"><?php _e('Art guides', 'arteforadomuseu'); ?></a></li>
 								<?php
 								$categories = get_categories();
 								if($categories) :
@@ -101,7 +101,7 @@
 									</li>
 								<?php endif; ?>-->
 							</ul>
-							<!--<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>-->
+							<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>
 						</nav>
 						<?php get_search_form(); ?>
 					</div>
@@ -110,3 +110,4 @@
 		</div>
 	</header>
 
+	
